c492e899e39ede81369eff35d09317b37076f761
root <root@mail.khe.local>
2025-08-26 18:18:06 +0700

    Daily log: 2025-08-26

---
+++ 2025-08-26/Leetcode.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        merged = []
+        
+        
+
+if __name__ == '__main__':
+    print(Solution.twoSum(Solution,[3,2,4], 6))
