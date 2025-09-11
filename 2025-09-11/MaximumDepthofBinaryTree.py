b2236838c3acc7edd7f2d6e6ace866f420f72044
root <root@mail.khe.local>
2025-09-11 15:56:40 +0700

    Daily log: 2025-09-11

---
+++ 2025-09-11/MaximumDepthofBinaryTree.py
@@ -0,0 +1,61 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    #----insert BST -----
+    def insertNode(self,root: Optional[TreeNode],val: int) -> Optional[TreeNode]:
+        if root is None:
+            root = TreeNode(val)
+
+        else:
+            if root.val < val:
+                root.right = self.insertNode(root.right,val)
+            elif root.val > val:
+                root.left = self.insertNode(root.left,val)
+            
+        return root
+
+    def LeftNodeRight(self, root: Optional[TreeNode]):
+        if root:
+            self.LeftNodeRight(root.left)
+            print(root.val, end=" ")
+            self.LeftNodeRight(root.right)
+
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is None:
+            return
+        else:
+            #Swap 2 values:
+            root.left, root.right = root.right, root.left
+            #Move to Left and continue swap
+            self.invertTree(root.left)
+            #Move to Right and continue swap
+            self.invertTree(root.right)
+        
+        return root
+    
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        else:
+            depth = 1 + max(self.maxDepth(root.left),self.maxDepth(root.right))
+
+        return depth
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    root = None
+    #insert ko theo BST
+    root = TreeNode(1)
+    root.left  = TreeNode(2)
+    root.right = TreeNode(3)
+    root.right.left = TreeNode(4)
+    
+    # print(sol.LeftNodeRight(root))
+    print(sol.maxDepth(root))
