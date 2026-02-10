ff77d052639f2fa9d5e4d3aeafc7b3349a45a5bb
lamluongkhe <84937864+lamluongkhe@users.noreply.github.com>
2026-02-10 23:36:35 +0700

    Add files via upload

---
+++ 2026-02-10/ms-a/person-impl/src/main/java/com/llk/impl/kafka/PersonKafkaProducer.java
@@ -0,0 +1,75 @@
+package com.llk.impl.kafka;
+
+import com.google.gson.Gson;
+import com.llk.api.Person;
+import com.llk.api.PersonEvent;
+import com.llk.api.PersonEventProducer;
+import com.llk.api.PersonEventType;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class PersonKafkaProducer implements PersonEventProducer{
+
+    private KafkaProducer<String, String> producer;
+    private final Gson gson = new Gson();
+    private String bootstrapServers;
+    private String topic;
+
+    public void setBootstrapServers(String bootstrapServers) {
+        this.bootstrapServers = bootstrapServers;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public void init() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        producer = new KafkaProducer<>(props);
+        System.out.println("Kafka Producer INIT OK");
+    }
+
+    public void destroy() {
+        if (producer != null) producer.close();
+    }
+
+    @Override
+    public void sendCreate(Person person) {
+        send(PersonEventType.CREATE, person);
+    }
+
+    @Override
+    public void sendUpdate(Person person) {
+        send(PersonEventType.UPDATE, person);
+    }
+
+    @Override
+    public void sendDelete(Person person) {
+        if (person == null || person.getId() <= 0) return;
+
+        PersonEvent event = new PersonEvent(PersonEventType.DELETE, person.getId());
+
+        producer.send(new ProducerRecord<>(
+                topic,
+                String.valueOf(person.getId()),
+                gson.toJson(event)
+        ));
+
+    }
+
+    private void send(PersonEventType type, Person person) {
+        PersonEvent event = new PersonEvent(type, person);
+        producer.send(new ProducerRecord<>(
+                topic,
+                String.valueOf(person.getId()),
+                gson.toJson(event)
+        ));
+    }
+}
